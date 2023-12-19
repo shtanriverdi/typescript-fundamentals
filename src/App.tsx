cf2b9434abe8ca57750f9888e07d670652304186
shtanriverdi <shtanriverdi@yahoo.com>
2023-12-19 12:16:14 +0300

    Aloow null type for state

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,19 @@ function App() {
   /* This count can be number or null
     useState<number | null>
   */
-  const [count, setCount] = useState<number | null>(0)
+  const [sampleState, setSampleState] = useState<string | null>("0")
+  const handleSampleState = () => {
+    setSampleState(null);
+  }
+
+  const [count, setCount] = useState<number>(0)
 
   const handleCount = () => {
     setCount(count + 1);
   }
 
   const handleReset = () => {
-    setCount(null);
+    setCount(0);
   }
 
   return (
@@ -20,6 +25,7 @@ function App() {
       <h1>{count}</h1>
       <button onClick={handleCount}>Increment</button>
       <button onClick={handleReset}>Reset</button>
+      <p onClick={handleSampleState}>{sampleState}</p>
     </>
   )
 }
